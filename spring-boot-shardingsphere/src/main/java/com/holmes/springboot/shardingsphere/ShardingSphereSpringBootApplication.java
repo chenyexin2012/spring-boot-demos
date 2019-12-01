d718439092d98bcebce4d7ec2175b8bc1fec3e77
chenyexin2012 <chenyexin2012@126.com>
2019-12-01 22:01:14 +0800

    add sharding sphere

---
+++ spring-boot-shardingsphere/src/main/java/com/holmes/springboot/shardingsphere/ShardingSphereSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.holmes.springboot.shardingsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingSphereSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingSphereSpringBootApplication.class, args);
+    }
+}
